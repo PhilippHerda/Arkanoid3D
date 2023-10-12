be7bda11bbe8ac10b7287794a6325d43495222b3
Philipp Herda <philipp.herda@cleanercode.de>
2023-10-12 17:32:47 +0200

    Make PowerUps spawn from destroyed blocks

--- Assets/Scripts/BlockBehaviour.cs
+++ Assets/Scripts/BlockBehaviour.cs
@@ -8,6 +8,7 @@ public class BlockBehaviour : MonoBehaviour
 
     public Material yellow;
     public Material green;
+    public GameObject powerUpPrefab;
 
     // Start is called before the first frame update
     void Start()
@@ -23,18 +24,21 @@ public class BlockBehaviour : MonoBehaviour
 
     private void OnTriggerEnter(Collider other)
     {
-        lives -= 1;
-        if (lives == 2)
+        if (other.transform.CompareTag("Ball"))
         {
-            GetComponent<MeshRenderer>().material = yellow;
-        }
-        else if (lives == 1)
-        {
-            GetComponent<MeshRenderer>().material = green;
-        }
-        else if (lives == 0)
-        {
-            // TODO: spawn powerup
+            lives -= 1;
+            if (lives == 2)
+            {
+                GetComponent<MeshRenderer>().material = yellow;
+            }
+            else if (lives == 1)
+            {
+                GetComponent<MeshRenderer>().material = green;
+            }
+            else if (lives == 0)
+            {
+                Instantiate(powerUpPrefab, transform.position, Quaternion.Euler(new Vector3(0, 0, 90)));
+            }
         }
     }
 
